d93fcf8f7ea4627ed7b60c66f5d6872ef2877eff
Aladin Khantouch <71331779+khantouch@users.noreply.github.com>
2023-02-17 01:33:47 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	/* Print the desired string to the standard output */
-	puts("Programming is like building a multilingual puzzle,");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	/* Return 0 to indicate success */
 	return (0);
