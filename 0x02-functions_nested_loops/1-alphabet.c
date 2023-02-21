83e5bd08b37d5c5b2eeeca22b1251e266b84fa8c
Khantouch <alakhantouche@gmail.com>
2023-02-21 15:19:57 -0800

    second task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - Enty point
+ * Return: always 0 (success)
+ */
+void main(void)
+{
+	char x;
+
+	for(a = 'a'; a<='z'; a++)
+	{
+		_putchar(x);
+	}
+
+	_putchar('\n');
+}
+
